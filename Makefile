6c78d8d1cb20102efd8231afc9403af453fdea46
shawnps <shawn@signalsciences.com>
2017-07-27 12:42:15 +0900

    add first unit test

--- Makefile
+++ Makefile
@@ -3,10 +3,13 @@ install:
 	curl -L https://git.io/misspell | bash
 lint: 
 	pylint SigSci.py
-	flake8 SigSci.py --ignore=E501
+	flake8 SigSci.py test_SigSci.py --ignore=E501
 
 reformat:
 	autopep8 --in-place --aggressive --aggressive --ignore=E501 SigSci.py
 
 misspell:
 	./bin/misspell SigSci.py
+
+test:
+	nosetests --with-coverage --cover-package=SigSciApiPy
---
+++ test_SigSci.py
@@ -0,0 +1,51 @@
+from __future__ import print_function
+import unittest
+import mock
+
+from SigSciApiPy.SigSci import SigSciAPI
+
+
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse(object):
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+            # TODO: add an actual "next" URI to make sure looping works etc.
+            self.text = '{"next": {"uri": ""}, "data": {"id": "testid", "serverHostname": "testhost"}}'
+
+        def json(self):
+            return self.json_data
+
+    return MockResponse({"key1": "value1"}, 200)
+
+
+def mocked_requests_post(*args, **kwargs):
+    class MockResponse(object):
+        def __init__(self, json_data, status_code):
+            self.status_code = status_code
+            self.json_data = json_data
+            self.cookies = {}
+
+        def json(self):
+            return self.json_data
+
+    return MockResponse({"token": "testtoken"}, 200)
+
+
+class TestSigSciAPI(unittest.TestCase):
+
+    @mock.patch("requests.get", side_effect=mocked_requests_get)
+    @mock.patch("requests.post", side_effect=mocked_requests_post)
+    def test_fetch(self, mock_get, mock_post):
+        # Assert requests.get calls
+        sigsci = SigSciAPI()
+        sigsci.email = "testemail"
+        sigsci.password = "testpass"
+        sigsci.corp = "testcorp"
+        sigsci.site = "testsite"
+        sigsci.authenticate()
+        sigsci.get_feed_requests()
+
+
+if __name__ == "__main__":
+    unittest.main()
